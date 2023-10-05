2ab274312a876a42f6d00720571082d9864d24a5
Antonio Florentino Junior <zasourounij@gmail.com>
2023-10-05 23:59:58 +0000

    Finalizando a parte 4 django_admin

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,34 +1,68 @@
 from django.contrib import admin
-from catalog.models import Author, Genre, Book, BookInstance
 
-# admin.site.register(Book)
-# admin.site.register(Author)
+# Register your models here.
+
+from .models import Author, Genre, Book, BookInstance #, Language
+
+"""Minimal registration of Models.
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(BookInstance)
 admin.site.register(Genre)
-# admin.site.register(BookInstance)
+admin.site.register(Language)
+"""
 
-# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(Language)
 
-# Define the admin class
+
+class BooksInline(admin.TabularInline):
+    """Defines format of inline book insertion (used in AuthorAdmin)"""
+    model = Book
+
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    """Administration object for Author models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - orders fields in detail view (fields),
+       grouping the date fields horizontally
+     - adds inline addition of books in author view (inlines)
+    """
+    list_display = ('last_name',
+                    'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
-# Register the admin class with the associated model
-admin.site.register(Author, AuthorAdmin)
 
-# admin.site.register(Book)
-# admin.site.register(BookInstance)
+class BooksInstanceInline(admin.TabularInline):
+    """Defines format of inline book instance insertion (used in BookAdmin)"""
+    model = BookInstance
 
-#Register the Admin classes for Book using the decorator
-@admin.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
+    """Administration object for Book models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - adds inline addition of book instances in book view (inlines)
+    """
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 
-# Register the Admin classes for BookInstance using the decorator
+admin.site.register(Book, BookAdmin)
+
+
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    list_display = ('book','status', 'due_back','id')
+    """Administration object for BookInstance models.
+    Defines:
+     - fields to be displayed in list view (list_display)
+     - filters that will be displayed in sidebar (list_filter)
+     - grouping of fields into sections (fieldsets)
+    """
+    list_display = ('book', 'status', 'due_back', 'id')
     list_filter = ('status', 'due_back')
 
     fieldsets = (
@@ -39,3 +73,45 @@ class BookInstanceAdmin(admin.ModelAdmin):
             'fields': ('status', 'due_back')
         }),
     )
+
+# from django.contrib import admin
+# from catalog.models import Author, Genre, Book, BookInstance
+
+# # admin.site.register(Book)
+# # admin.site.register(Author)
+# admin.site.register(Genre)
+# # admin.site.register(BookInstance)
+
+# # admin.site.register(Author)
+
+# # Define the admin class
+# class AuthorAdmin(admin.ModelAdmin):
+#     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+#     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+# # Register the admin class with the associated model
+# admin.site.register(Author, AuthorAdmin)
+
+# #Register the Admin classes for Book using the decorator
+# @admin.register(Book)
+# class BookAdmin(admin.ModelAdmin):
+#     list_display = ('title', 'author', 'display_genre')
+
+# class BooksInstanceInline(admin.TabularInline):
+#     model = BookInstance
+
+# # Register the Admin classes for BookInstance using the decorator
+# @admin.register(BookInstance)
+# class BookInstanceAdmin(admin.ModelAdmin):
+#     list_display = ('book','status', 'due_back','id')
+#     list_filter = ('status', 'due_back')
+#     inlines = [BooksInstanceInline]
+
+#     fieldsets = (
+#         (None, {
+#             'fields': ('book', 'imprint', 'id')
+#         }),
+#         ('Availability', {
+#             'fields': ('status', 'due_back')
+#         }),
+#     )
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -11,6 +11,11 @@ class Genre(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+
+class Language(models.Model):
+    """Model representing a Language (e.g. English, French, Japanese, etc.)"""
+    name = models.CharField(max_length=200,
+                            help_text="Enter the book's natural language (e.g. English, French, Japanese etc.)")
     
 
 
